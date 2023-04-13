d550ca25cc20503f2be96db43fadc76d51ab9fb6
Nguyễn Thái Phương <nguyenphuongmkey@gmail.com>
2023-04-13 20:22:28 +0700

    init source

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Socialite;
+use App\User;
+use Auth;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function login(Request $request){
+        $data= $request->all();
+
+        $query = User::query();
+        $user = $query->whereRaw("email ='" . $data['email']. "'")
+            ->whereRaw("password ='" . $data['password'] . "'")
+            ->whereRaw("status = 'active'")
+            ->first();
+//        dd($user);
+//        dd($query->toSql());
+
+        if ($user) {
+            Auth::login($user);
+            Session::put('user',$data['email']);
+            request()->session()->flash('success','Successfully login');
+            return redirect()->route('home');
+        } else {
+            request()->session()->flash('error','Invalid email and password pleas try again!');
+            return redirect()->back();
+        }
+    }
+
+    public function credentials(Request $request){
+        return ['email'=>$request->email,'password'=>$request->password,'status'=>'active','role'=>'admin'];
+    }
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirect($provider)
+    {
+        // dd($provider);
+     return Socialite::driver($provider)->redirect();
+    }
+
+    public function Callback($provider)
+    {
+        $userSocial =   Socialite::driver($provider)->stateless()->user();
+        $users      =   User::where(['email' => $userSocial->getEmail()])->first();
+        // dd($users);
+        if($users){
+            Auth::login($users);
+            return redirect('/')->with('success','You are login from '.$provider);
+        }else{
+            $user = User::create([
+                'name'          => $userSocial->getName(),
+                'email'         => $userSocial->getEmail(),
+                'image'         => $userSocial->getAvatar(),
+                'provider_id'   => $userSocial->getId(),
+                'provider'      => $provider,
+            ]);
+         return redirect()->route('home');
+        }
+    }
+}
